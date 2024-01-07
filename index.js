efd762cca4f1fedb816c13ec1283fcab4d7a5a92
Federico Merino <fedemerino00@gmail.com>
2024-01-07 02:42:09 -0300

    post-deploy commit

--- index.js
+++ index.js
@@ -178,6 +178,7 @@ const getUrl = (page) => {
   return `${BASE_URL}/${CITY}-${MIN_PRICE}-${MAX_PRICE}-${FILTERS}-${page}.html`;
 };
 
+
 cron.schedule("*/1 * * * *", () => {
   writeApartmentsInGoogleSheet();
-})
+});
